32b661f2bf9b5ebf58f5b8565c5211fe03220817
fabiantax87 <fabiantax87@users.noreply.github.com>
2022-05-23 14:21:14 +0200

    added packages and setup react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 import React from "react";
-import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import "./App.scss";
+import Home from "./views/Home/Home";
+import Layout from "./views/Layout/Layout";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
