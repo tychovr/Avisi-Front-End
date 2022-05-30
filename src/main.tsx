b3aaaffabebfa234b6cb5809aed74619af60f03c
fabiantax87 <fabiantax87@users.noreply.github.com>
2022-05-30 14:24:52 +0200

    real init

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import "./index.scss";
 import App from "./App";
+import "./main.scss";
 
-const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
-root.render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
