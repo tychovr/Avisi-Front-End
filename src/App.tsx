e9e1c1686a5a5b0aea630b012d8a8ce29d4a1719
Yigit Findik <yigit8@live.nl>
2022-06-28 11:17:13 +0200

    Layout, wg-landingpage
    Layout aangepast zodat ik kan navigeren en eigen routes gemaakt

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import "./App.scss";
 import Home from "./views/Home/Home";
 import Layout from "./views/Layout/Layout";
+import Wglandingpage from "./views/Werkgever/landingpage/wg-landingpage";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
+          <Route path="werkgever" element={<Wglandingpage />} />
         </Route>
       </Routes>
     </div>
