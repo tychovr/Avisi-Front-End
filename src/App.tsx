ec95027c063fef6c4e409d1f50fee5c8f0aa0160
valentijnoa <74962322+valentijnoa@users.noreply.github.com>
2022-06-30 09:39:26 +0200

    profile page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import VacancySearch from "./views/VacancySearch/VacancySearch";
 import Wglandingpage from "./views/Werkgever/landingpage/wg-landingpage";
 import "./App.scss";
 import Home from "./views/Home/Home";
@@ -11,18 +10,6 @@ function App() {
   return (
     <div className="App">
       <Profile />
-      <Routes>
-        <Route path="/" element={<Layout />}></Route>
-
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-
-          <Route path="werkgever" element={<Wglandingpage />} />
-          <Route path="Profile" element={<Profile />} />
-
-          <Route path="/vacancy-search" element={<VacancySearch />} />
-        </Route>
-      </Routes>
     </div>
   );
 }
--- src/views/Layout/Layout.tsx
+++ src/views/Layout/Layout.tsx
@@ -1,16 +1,11 @@
-<<<<<<< HEAD
 import React from "react";
 import "./Layout.scss";
 import { Outlet, Link } from "react-router-dom";
-=======
 import Navigation from "../../components/Navigation/Navigation";
-import { Outlet } from "react-router-dom";
->>>>>>> 5a26f878183d4edb4e1ecaaf8dde18865e4ea55a
 
 const Layout = () => {
   return (
     <>
-<<<<<<< HEAD
       <ul>
         <li>
           <Link to="/">Home</Link>
@@ -19,9 +14,8 @@ const Layout = () => {
           <Link to="/werkgever">Werkgever</Link>
         </li>
       </ul>
-=======
+
       <Navigation />
->>>>>>> 5a26f878183d4edb4e1ecaaf8dde18865e4ea55a
       <Outlet />
     </>
   );
