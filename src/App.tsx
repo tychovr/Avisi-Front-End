c3c780aaf8f806a5e11bc6296e86d710d09bf884
tychovr <68756557+tychovr@users.noreply.github.com>
2022-06-30 11:30:04 +0200

    Revert "Merge pull request #14 from tychovr/Werkgever"
    This reverts commit 673cfac8c367fc922760859739558537b9306bb9, reversing
    changes made to 5a26f878183d4edb4e1ecaaf8dde18865e4ea55a.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Wglandingpage from "./views/Werkgever/landingpage/wg-landingpage";
+import VacancySearch from "./views/VacancySearch/VacancySearch"
 import "./App.scss";
 import Home from "./views/Home/Home";
 import Layout from "./views/Layout/Layout";
-import Profile from "./views/Werkgever/profile/Profile";
 
 function App() {
   return (
     <div className="App">
-      <Profile />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/vacancy-search" element={<VacancySearch/>} />
+        </Route>
+      </Routes>
     </div>
   );
 }
