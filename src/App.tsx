5d0eaedb1fadb12fca26878735aa3530afa2bd6e
valentijnoa <74962322+valentijnoa@users.noreply.github.com>
2022-06-30 09:31:16 +0200

    werkgever

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,27 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import VacancySearch from "./views/VacancySearch/VacancySearch"
+import VacancySearch from "./views/VacancySearch/VacancySearch";
+import Wglandingpage from "./views/Werkgever/landingpage/wg-landingpage";
 import "./App.scss";
 import Home from "./views/Home/Home";
 import Layout from "./views/Layout/Layout";
-<<<<<<< HEAD
 import Profile from "./views/Werkgever/profile/Profile";
-=======
-import Wglandingpage from "./views/Werkgever/landingpage/wg-landingpage";
->>>>>>> e9e1c1686a5a5b0aea630b012d8a8ce29d4a1719
 
 function App() {
   return (
     <div className="App">
       <Profile />
       <Routes>
-      <<<<<<< HEAD
         <Route path="/" element={<Layout />}></Route>
-=======
+
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
 
-          <<<<<<< HEAD
           <Route path="werkgever" element={<Wglandingpage />} />
-=======
-          <Route path="/vacancy-search" element={<VacancySearch/>} />
->>>>>>> 5a26f878183d4edb4e1ecaaf8dde18865e4ea55a
+          <Route path="Profile" element={<Profile />} />
+
+          <Route path="/vacancy-search" element={<VacancySearch />} />
         </Route>
-        >>>>>>> e9e1c1686a5a5b0aea630b012d8a8ce29d4a1719
       </Routes>
     </div>
   );
--- src/views/Werkgever/profile/Profile.jsx
+++ src/views/Werkgever/profile/Profile.jsx
@@ -51,39 +51,21 @@ const Profile = () => {
         </div>
         <div className="bottom-about">
           <p className="card-title">About</p>
+          <p className="about-text">
+            Dummy text is text that is used in the publishing industry or by web
+            designers to occupy the space which will later be filled with 'real'
+            content. This is required when, for example, the final text is not
+            yet available. Dummy text is also known as 'fill text'. It is said
+            that song composers of the past used dummy texts as lyrics when
+            writing melodies in order to have a 'ready-made' text to sing with
+            the melody. Dummy texts have been in use by typesetters since the
+            16th century.
+          </p>
         </div>
         <div className="bottom-card">
           <p className="card-title">Placeholder</p>
         </div>
       </div>
-      {/* <Navbar />
-      <div className="profile-container">
-        <div className="left-container">
-          <div className="img-container">
-            <img src={profilepic} alt="" />
-          </div>
-          <div className="profile-left">
-            <h3>Account</h3>
-            <p className="profile-left-info">Username</p>
-            <p className="profile-left-info">Email</p>
-            <p className="profile-left-info">Full name</p>
-            <p>Title</p>
-          </div>
-        </div>
-        <div className="right-container">
-          <div className="profile-right">
-            <h1>John Doe</h1>
-            <h3>johndoe@gmail.com</h3>
-            <p> - Title</p>
-          </div>
-          <div className="profile-right-info">
-            <p>John Doe</p>
-            <p>johndoe@gmail</p>
-            <p>John Doe</p>
-            <p>Title</p>
-          </div>
-        </div>
-      </div> */}
     </div>
   );
 };
